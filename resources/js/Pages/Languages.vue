995a71e76dd0415398698d9dbe3ace34bbb8f1d0
Julien Steininger <julien.steininger@gmx.de>
2024-03-22 15:59:39 +0100

    Basic compatibility for smartphones

--- resources/js/Pages/Languages.vue
+++ resources/js/Pages/Languages.vue
@@ -9,7 +9,7 @@
             <li class="grid grid-cols-3 md:grid-cols-11 gap-2 items-center">
                 <span class="font-bold">Deutsch:</span>
                 <span
-                    class="bg-green-600 py-1 px-2 rounded-md w-64 block h-8 shadow-md"
+                    class="bg-green-600 py-1 px-2 rounded-md w-48 md:w-64 block h-8 shadow-md"
                 >
                     Muttersprache
                 </span>
@@ -17,7 +17,7 @@
             <li class="grid grid-cols-3 md:grid-cols-11 gap-2 items-center">
                 <span class="font-bold">Englisch:</span>
                 <span
-                    class="relative w-64 py-1 block rounded-md bg-slate-800 h-8 shadow-md"
+                    class="relative w-48 md:w-64 py-1 block rounded-md bg-slate-800 h-8 shadow-md"
                 >
                     <span
                         class="bg-green-600 w-2/3 h-full top-0 rounded-md block absolute flex items-center"
@@ -29,7 +29,7 @@
             <li class="grid grid-cols-3 md:grid-cols-11 gap-2 items-center">
                 <span class="font-bold">Japanisch:</span>
                 <span
-                    class="relative w-64 py-1 block rounded-md bg-slate-800 h-8 shadow-md"
+                    class="relative w-48 md:w-64 py-1 block rounded-md bg-slate-800 h-8 shadow-md"
                 >
                     <span
                         class="bg-green-600 w-1/3 h-full top-0 rounded-md block absolute flex items-center"
